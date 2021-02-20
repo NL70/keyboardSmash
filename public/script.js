bbd4ccc1ff11fad2c2fcbf4d05ee35f0d5ca012c
Nigel Lee <nindustriestech@gmail.com>
2021-02-20 13:30:29 +0800

    Fixing errors

--- public/script.js
+++ public/script.js
@@ -1,10 +1,15 @@
-let books;
+let books = [];
 let searchTerm = "";
 
 const fetchBooks = async () => {
-  books = await fetch(
+  if (!searchTerm) {
+    return;
+  }
+
+  response = await fetch(
     `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=AIzaSyDNcGyk57dWBtic9VgXWNRzWOrf90PX7gU`
   ).then((res) => res.json());
+  books = response.items || [];
 };
 
 const results = document.getElementById("results");
@@ -13,10 +18,8 @@ const input = document.querySelector("#search");
 const showBooks = async () => {
   results.innerHTML = "";
   await fetchBooks();
-  console.log(books.items);
   // getting the data
-
-  const searchedBooks = books.items.filter((book) =>
+  const searchedBooks = books.filter((book) =>
     book.volumeInfo.title.toLowerCase().includes(searchTerm)
   );
   const ul = document.createElement("ul");
@@ -26,9 +29,19 @@ const showBooks = async () => {
     const h2 = document.createElement("h2");
     const p = document.createElement("p");
     const div = document.createElement("div");
-    img.setAttribute("src", element.volumeInfo.imageLinks.thumbnail);
+    if (
+      element.volumeInfo.imageLinks &&
+      element.volumeInfo.imageLinks.thumbnail
+    ) {
+      img.setAttribute("src", element.volumeInfo.imageLinks.thumbnail);
+    } else {
+      img.setAttribute(
+        "src",
+        "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081"
+      );
+    }
     h2.innerText = element.volumeInfo.title;
-    p.innerText = element.volumeInfo.description;
+    p.innerText = element.volumeInfo.description || "No description";
     li.appendChild(img);
     div.appendChild(h2);
     div.appendChild(p);
